eb4eb48df402c8c60cac73458a85cf9465cbf0d2
Johnathon Wood <jaywood128@gmail.com>
2018-11-09 15:04:58 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,11 +1,23 @@
+
 class School
-
-attr_accesor :roster
-
+    attr_accessor :roster
   def initialize(name)
     @name = name
-    @roster = []
+    @roster = {}
   end
-
-
+  def add_student(student_name, grade)
+    @roster[grade] ||= []
+    @roster[grade].push(student_name)
+  end
+  def grade(student_grade)
+    @roster[student_grade]
+  end
+  def sort
+    sorted_hash = {}
+    @roster.each do |grade, student_names|
+    
+      sorted_hash[grade] = student_names.sort
+    end
+    sorted_hash
+  end
 end
